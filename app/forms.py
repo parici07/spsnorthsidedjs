72f3df655fa058d70e4fed26e07abda92a7aebe6
paricisps <25217@stpauls.qld.edu.au>
2023-09-04 12:28:38 +1000

    .

--- app/forms.py
+++ app/forms.py
@@ -5,14 +5,14 @@ from wtforms.validators import ValidationError, Email, EqualTo, DataRequired, Le
 from app.models import User, Events
 
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm): # Login form for existing users
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('SIGN IN')
 
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm): # Registration form for new users
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -20,68 +20,68 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('REGISTER')
 
-    def validate_username(self, username):
+    def validate_username(self, username): # Checks if username already exists in database
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
 
-    def validate_email (self, email):
+    def validate_email (self, email): # Checks if email already exists in database
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
 
 
-class EditProfileForm(FlaskForm):
+class EditProfileForm(FlaskForm): # Edit profile form for existing users
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-class UploadPfpForm(FlaskForm):
+class UploadPfpForm(FlaskForm): # Upload profile picture form for existing users
     pfp = FileField('Profile Picture', validators=[DataRequired()])
     submit = SubmitField('Upload')
 
-class CreateEventForm(FlaskForm):
+class CreateEventForm(FlaskForm): # Create event form for existing users
     event_name = StringField('Event Name', validators=[DataRequired()])
     event_code = StringField('Event Code', validators=[DataRequired()])
     event_location = StringField('Event Location', validators=[DataRequired()])
     event_description = TextAreaField('Event Description', validators=[Length(min=0, max=140)])
     submit = SubmitField('Create Event')
 
-    def validate_event_name(self, event_name):
+    def validate_event_name(self, event_name): # Checks if event name already exists in database
         event = Events.query.filter_by(event_name=event_name.data).first()
         if event is not None:
             raise ValidationError('Please use a different event name.')
 
-    def validate_event_code(self, event_code):
+    def validate_event_code(self, event_code): # Checks if event code already exists in database
         event = Events.query.filter_by(event_code=event_code.data).first()
         if event is not None:
             raise ValidationError('Please use a different event code.')
 
-class FavouriteSongForm(FlaskForm):
+class FavouriteSongForm(FlaskForm): # Favourite song form for existing users
     song_name = StringField('Song Name', validators=[DataRequired()])
     artist_name = StringField('Artist Name', validators=[DataRequired()])
     track_id = StringField('Track ID', validators=[DataRequired()])
     submit = SubmitField('Favourite')
 
 
-class VoteSongForm(FlaskForm):
+class VoteSongForm(FlaskForm): # Vote song form for existing users
     song_name = StringField('Song Name', validators=[DataRequired()])
     artist_name = StringField('Artist Name', validators=[DataRequired()])
     track_id = StringField('Track ID', validators=[DataRequired()])
     submit = SubmitField('Vote')
 
 
-class JoinEventForm(FlaskForm):
+class JoinEventForm(FlaskForm): # Join event form for existing users
     event_code = StringField('Event Code', validators=[DataRequired()])
     submit = SubmitField('Search')
 
-class SongReviewForm(FlaskForm):
+class SongReviewForm(FlaskForm): # Song review form for existing users
     review = TextAreaField('Review', validators=[Length(min=0, max=140), DataRequired()])
     user_id = StringField('User ID', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     song_id = StringField('Song ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class SearchUsersForm(FlaskForm):
+class SearchUsersForm(FlaskForm): # Search users form for existing users
     username = StringField('Username', validators=[DataRequired()])
     submit = SubmitField('Search')
--- app/models.py
+++ app/models.py
@@ -4,7 +4,9 @@ from flask_login import UserMixin
 from app import login
 
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model): # model for the user table
+
+    # information about the user
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -13,6 +15,7 @@ class User(UserMixin, db.Model):
     in_event = db.Column(db.Boolean, default=False)
     pfp = db.Column(db.String(140))
 
+    # relationships to other tables
     events = db.relationship('Events', back_populates='user')
     songs = db.relationship('FavouriteSong', back_populates='user')
     event_users = db.relationship('EventUsers', back_populates='user')
@@ -20,19 +23,20 @@ class User(UserMixin, db.Model):
     song_reviews = db.relationship('SongReviews', back_populates='user', foreign_keys='SongReviews.user_id')
     username_rel = db.relationship('SongReviews', back_populates='username_rel', foreign_keys='SongReviews.username')
 
-    def get_id(self):
+    def get_id(self): # returns the user id as a string
         return str(self.user_id)
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the user
         return '<User {}>'.format(self.username)
 
-    def set_password (self, password):
+    def set_password (self, password): # sets the password hash
         self.password_hash = generate_password_hash(password)
 
-    def check_password (self, password):
+    def check_password (self, password): # checks the password hash
         return check_password_hash(self.password_hash, password)
 
-class Events(db.Model):
+class Events(db.Model): # model for the events table
+    # information about the event
     event_id = db.Column(db.Integer, primary_key=True, unique=True)
     event_name = db.Column(db.String(64), index=True, unique=True)
     event_code = db.Column(db.Integer, unique=True)
@@ -40,88 +44,102 @@ class Events(db.Model):
     event_location = db.Column(db.String(140), index=True)
     event_description = db.Column(db.String(140), index=True)
 
+    # information with foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', name='fk_user_id'))
 
+    # relationships to other tables
     user = db.relationship('User', back_populates='events')
     event_users = db.relationship('EventUsers', back_populates='event')
     voted_songs = db.relationship('VotedSongs', back_populates='event')
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the event
         return '<Event {}>'.format(self.event_name)
 
-    def get_id(self):
+    def get_id(self): # returns the event id as a string
         return str(self.event_id)
 
 
-class FavouriteSong(db.Model):
+class FavouriteSong(db.Model): # model for the favourite songs table
+    # information about the song
     song_id = db.Column(db.Integer, primary_key=True, unique=True)
     track_id = db.Column(db.Integer, index=True)
     song_name = db.Column(db.String(64), index=True)
     artist_name = db.Column(db.String(64), index=True)
 
+    # information with foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', name='songs_user_id'))
 
+    # relationships to other tables
     user = db.relationship('User', back_populates='songs')
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the song
         return '<Song {}>'.format(self.song_name)
 
-    def get_id(self):
+    def get_id(self): # returns the song id as a string
         return str(self.song_id)
 
 
-class EventUsers(db.Model):
+class EventUsers(db.Model): # model for the event users table
+    # information about the event user
     event_user_id = db.Column(db.Integer, primary_key=True, unique=True)
     is_admin = db.Column(db.Boolean, default=False)
 
+    # information with foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', name='event_users_user_id'))
     event_id = db.Column(db.Integer, db.ForeignKey('events.event_id', name='event_users_event_id'))
 
+    # relationships to other tables
     user = db.relationship('User', back_populates='event_users')
     event = db.relationship('Events', back_populates='event_users')
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the event user
         return '<EventUser {}>'.format(self.event_user_id)
 
-    def get_id(self):
+    def get_id(self): # returns the event user id as a string
         return str(self.event_user_id)
 
-class VotedSongs(db.Model):
+class VotedSongs(db.Model): # model for the voted songs table
+    # information about the voted song
     vote_id = db.Column(db.Integer, primary_key=True, unique=True)
     track_id = db.Column(db.Integer, index=True)
     song_name = db.Column(db.String(64), index=True)
     artist_name = db.Column(db.String(64), index=True)
 
+    # information with foreign keys
     event_id = db.Column(db.Integer, db.ForeignKey('events.event_id', name='voted_songs_event_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', name='voted_songs_user_id'))
 
+    # relationships to other tables
     event = db.relationship('Events', back_populates='voted_songs')
     user = db.relationship('User', back_populates='voted_songs')
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the voted song
         return '<VotedSongs {}>'.format(self.vote_id)
 
-    def get_id(self):
+    def get_id(self): # returns the voted song id as a string
         return str(self.vote_id)
 
-class SongReviews(db.Model):
+class SongReviews(db.Model): # model for the song reviews table
+    # information about the song review
     review_id = db.Column(db.Integer, primary_key=True, unique=True)
     review = db.Column(db.String(140), index=True)
     reviewsong_id = db.Column(db.Integer, index=True)
 
+    # information with foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', name='song_reviews_user_id'))
     username = db.Column(db.String(64), db.ForeignKey('user.username', name='song_reviews_username'))
 
+    # relationships to other tables
     user = db.relationship('User', back_populates='song_reviews', foreign_keys=user_id)
     username_rel = db.relationship('User', back_populates='song_reviews', foreign_keys=username)
 
-    def __repr__(self):
+    def __repr__(self): # returns a string representation of the song review
         return '<SongReviews {}>'.format(self.review_id)
 
-    def get_id(self):
+    def get_id(self): # returns the song review id as a string
         return str(self.review_id)
 
 
-@login.user_loader
+@login.user_loader # loads the user
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(int(user_id)) # returns the user id as an integer
